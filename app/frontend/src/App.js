eb452df0a8a354bba22fa1a9ea9f929846ef9956
Luke Luo <luke.luo@refinitiv.com>
2020-05-23 17:51:00 +0800

    fix3

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,7 +1,10 @@
 import './style.scss'
 import React from 'react'
 
-export default function App({ a }) {
+export default function App() {
 
-  return <>Hello App</>;
+
+
+
+  return <>Hello App</>
 }
